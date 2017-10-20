a9c806fb5d8e64b650e9b30ae05c70f7a7c924f8
Renan Avrahami <renan.avrahami@gmail.com>
2017-10-19 21:06:33 -0400

    Add Controllers Add GoogleService-Info

---
+++ ChitChat/Model/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  ChitChat
+//
+//  Created by Renan Avrahami on 10/19/17.
+//  Copyright © 2017 Renan Avrahami. All rights reserved.
+//
+//  This is the model class that represents the blueprint for a message
+
+
+class Message {
+    
+    
+    
+}
