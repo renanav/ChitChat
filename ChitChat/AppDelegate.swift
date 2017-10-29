cb355b5ee131618a549386aedc3fb3e7edd1ceb3
Renan Avrahami <renan.avrahami@gmail.com>
2017-10-28 22:41:12 -0400

    Fix bug that wiped FireBase DB

--- ChitChat/AppDelegate.swift
+++ ChitChat/AppDelegate.swift
@@ -16,9 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-            FIRApp.configure()
-        let myDataBase = FIRDatabase.database().reference()
-        myDataBase.setValue("Testing testing 123")
+        //Configure and initialize FireBase DB
+        FIRApp.configure()
+
         return true
     }
 
